fa66d6f17e5460837176b16625f1fb38017bcfda
Alan Schiojetman <alans@boltinc.com>
2023-04-28 16:07:35 +0300

    update db scheme

--- src/pages/api/expenses.ts
+++ src/pages/api/expenses.ts
@@ -3,9 +3,10 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type UsersAndGroups = {
   sum: number,
- category_name: string,
+  category_name: string,
   date_created:string,
 }
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -16,14 +17,13 @@ export default async function handler(
     return res.status(400).json({message:"Invalid group id"})
   }
 
-
   const userQuery = userId !== null ? `and fk_user_id ='${userId}'` : ''
   const filteredByCategories = await prisma.$queryRawUnsafe(`
-  select cast(sum(up.amount) as int)   ,pc.category_name
-  from userproducts up join productcategories pc on up.fk_category_id = pc.pk_id
+  select cast(sum(up.amount) as int)   ,up.category_name
+  from userproducts up
   where fk_group_id='${groupId}'
   ${userQuery}
-  group by pc.category_name
+  group by up.category_name
   `) as UsersAndGroups[]
 
   const categoryAndAmountDic:any[] = [["category","amount"]]
@@ -33,5 +33,7 @@ export default async function handler(
     const category = d.category_name
     categoryAndAmountDic.push([category,amount])
   })
+
 res.status(200).json({categoryAndAmountDic})
+
 }
